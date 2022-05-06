817117ed9d2b4d9e6a52671539f96b93f804ed62
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-05-05 21:00:29 -0300

    atv. Faculdade - organizando

---
+++ src/com/poo/Trimestre1/Atividades/Atividade02/Q3/Pais.java
@@ -0,0 +1,14 @@
+package com.poo.Trimestre1.Atividades.Atividade02.Q3;
+
+public class Pais {
+//03 - Escreva uma classe que represente um país. Um país é representado através dos atributos: sigla (ex.: BRA), nome (ex.:
+//    Brasil), população (ex.: 200.333.444) e a sua dimensão em Km2 (ex.: 8.515.767,049).
+//    Escreva a classe em Java contendo:
+//   Um método construtor que inicialize a sigla, o nome e a dimensão do país;
+//    Os métodos de acesso (getter/setter) para as propriedades sigla, nome, população e dimensão do país;
+//    Um método que retorne a densidade populacional do país;
+
+
+}
+
+
