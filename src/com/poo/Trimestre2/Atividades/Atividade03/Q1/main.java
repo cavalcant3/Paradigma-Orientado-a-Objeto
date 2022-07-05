606ac733b30c4d44f7240db48d90e31cc08eac29
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-07-05 10:29:05 -0300

    testando a saída randomica da consulta

--- src/com/poo/Trimestre2/Atividades/Atividade03/Q1/main.java
+++ src/com/poo/Trimestre2/Atividades/Atividade03/Q1/main.java
@@ -2,17 +2,12 @@ package com.poo.Trimestre2.Atividades.Atividade03.Q1;
 
 public class main {
     public static void main(String[] args) {
-//- Crie um classe derivada de Paciente de nome Conveniado (possui convênio de saúde) cujas instâncias são
-// caracterizadas pelos atributos int númeroConvênio e String nomeConvênio.
-//- Implemente na classe Conveniado o método Print() e agendarConsulta (String nomeEspecialidade, int númeroConvênio,
-// String nomeConvênio).
-//- Crie um classe derivada de Paciente de nome NãoConveniado cujas instâncias são caracterizadas pelos atributos
-// int numCartãoCrédito e String bandeiraCartãoCrédito.
-//- Implemente na classe NaoConveniado o método Print() e agendarConsulta (String nomeEspecialidade,
-// int numCartãoCrédito e String bandeiraCartãoCrédito).
+
 //- O método agandarConsulta() deve gerar uma data de consulta randômica válida para o ano de 2023 e retornar uma
 // String (concatenar especialidade e data da consulta).
+
 //- Na classe NaoConveniado, no método agendarConsulta () deve ser retornado também o valor da consulta (R$ 200,00).
+
 //Classe de teste:
 //- Elabore um programa de para criar 5 pacientes (Use o comando for. A variavel de controle deve ser chamada de X).
 //- Para X com valor ímpar crie objetos "Conveniado" e para valor par crie objetos "NãoConveniado".
@@ -24,5 +19,9 @@ public class main {
 //PACIENTE: ANA PAULA SÁ DA SILVA - SEM CONVÊNIO
 //	  CARDIOLOGISTA  - 15/03/2023 - R$ 200,00
 
+        Paciente paciente = new Paciente();
+        System.out.println(paciente.agendarConsulta("cardio",
+                999, "Unimed")
+        );
     }
 }
