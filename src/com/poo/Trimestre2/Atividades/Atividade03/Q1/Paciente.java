5ba345d6300af99e7b3083ced97b7c88e771f8f9
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-07-05 10:30:00 -0300

    lógica para data aleatória no ano de 2023

--- src/com/poo/Trimestre2/Atividades/Atividade03/Q1/Paciente.java
+++ src/com/poo/Trimestre2/Atividades/Atividade03/Q1/Paciente.java
@@ -1,13 +1,29 @@
 package com.poo.Trimestre2.Atividades.Atividade03.Q1;
 
+import java.util.GregorianCalendar;
+
 public class Paciente {
     int cpf;
     String nome;
 
-    public String print(){
+    public String print() {
         return null;
     }
-    public void agendarConsulta(String nomeEspecialidade,
-                                int númeroConvênio,
-                                String nomeConvênio){}
+
+    public String agendarConsulta(String nomeEspecialidade,
+                                  int númeroConvênio,
+                                  String nomeConvênio) {
+        GregorianCalendar gc = new GregorianCalendar();
+        int ano = randBetween(2023, 2023);
+        gc.set(gc.YEAR, ano);
+        int diaDoAno = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
+        gc.set(gc.DAY_OF_YEAR, diaDoAno);
+        String resultado = (gc.get(gc.YEAR) + "-" + (gc.get(gc.MONTH) + 1) + "-" + gc.get(gc.DAY_OF_MONTH));
+        return resultado;
+
+    }
+
+    public static int randBetween(int start, int end) {
+        return start + (int) Math.round(Math.random() * (end - start));
+    }
 }
